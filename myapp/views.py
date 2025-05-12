089892d1619684ca721fa8df19c1937bcdf1983e
KhanhLinh <linh105463@donga.edu.vn>
2025-05-12 16:42:29 +0700

    Thêm trang giới thiệu và chỉnh sửa template chính

---
+++ myapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'myapp/home.html')
+
+def about(request):
+    return render(request, 'myapp/about.html')
+def contact(request):
+    return render(request, 'myapp/contact.html')
